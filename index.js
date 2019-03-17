3dc6d8de3a8e3c8b75c775f3fac3eb811da6b281
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 21:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-function logDriverNames(drivers) {
+const logDriverNames = function(drivers) {
   return drivers.forEach(function(driver) {
     console.log(driver.name);})
 }
